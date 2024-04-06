d09dbb8ce53966df800e12375b81ebe8bce8d01a
Sean O'Donnell <saoski88@gmail.com>
2024-04-06 19:50:32 -0400

    Added PID controller class (untested)

--- sketch.js
+++ sketch.js
@@ -1,10 +1,60 @@
-var pSliderX = 500;
-var pSliderY = 50;
-var sliderDeltaY = 75;
-var pSlider;
-var iSlider;
-var dSlider;
-var font;
+class PIDController {
+  constructor(kP, kI, kD) {
+    this.kP = kP;
+    this.kI = kI;
+    this.kD = kD;
+    this.integratorRange = 300; // In frames (or the # of times you call calculate)
+    this.currentSetpoint = 0;
+    this.errorArray = [];
+    this.previousError;
+  }
+
+  calculate(current, target) {
+    // Calculate P output.
+    let error = target - current;
+    let pOutput = kP * error;
+
+    // Add the current error to our array and shift it if it is past our integrator range.
+    this.errorArray.push(error);
+    if (this.errorArray.length > this.integratorRange) {
+      this.errorArray.shift();
+    }
+
+    // Add together all the areas to calculate I output.
+    let errorArea = this.errorArray.reduce((total, amount) => total + amount);
+    let iOutput = kI * errorArea;
+
+    // Calculate slope for the D output.
+    let slope;
+    if (!this.previousError) { // If this is the first time calculate is being called.
+      slope = 0;
+    } else {
+      slope =  this.previousError - error; // If error is descreasing from positive direction, positive output will be made.
+    }
+    let dOutput = kD * slope;
+
+    return pOutput + iOutput + dOutput;
+  }
+
+  updateGains(kP, kI, kD) {
+    if (this.kP != kP || this.kI != kI || this.kD != kD) {
+      this.kP = kP;
+      this.kI = kI;
+      this.kD = kD;
+    }
+    // text(kP + " " + kI + " " + kD, 100, 100);
+  }
+}
+
+let pSliderX = 500;
+let pSliderY = 50;
+let sliderDeltaY = 75;
+let pSlider;
+let iSlider;
+let dSlider;
+let font;
+let setpointSlider;
+let pidController;
 
 function preload() {
   font = loadFont('fonts/Playfair.ttf')
@@ -13,28 +63,32 @@ function preload() {
 function setup() {
   createCanvas(800, 600);
   generatePIDSliders();
+  pidController = new PIDController(0, 0, 0);
 }
 
 function draw() {
   background(50, 50, 50);
   fill(255, 255, 255);
   textSize(20);
-  textFont(font)
+  textFont(font);
   text("kP: " + round(pSlider.value(), 2), pSliderX + 3, pSliderY - 10);
   text("kI: " + round(iSlider.value(), 2), pSliderX + 3, pSliderY + sliderDeltaY - 10);
   text("kD: " + round(dSlider.value(), 2), pSliderX + 3, pSliderY + sliderDeltaY * 2 - 10);
+  text("Setpoint: " + round(setpointSlider.value(), 2), pSliderX + 3, pSliderY + sliderDeltaY * 3 - 10);
+
+  pidController.updateGains(pSlider.value(), iSlider.value(), dSlider.value());
 }
 
-function configureSlider(xpos, ypos) {
-  let slider = createSlider(0, 100, 0, 0);
+function configureSlider(xpos, ypos, min, max, value) {
+  let slider = createSlider(min, max, value, 0);
   slider.position(xpos, ypos);
   slider.size(290);
   return slider;
 }
 
 function generatePIDSliders() {
-  pSlider = configureSlider(pSliderX, pSliderY);
-  iSlider = configureSlider(pSliderX, pSliderY + sliderDeltaY);
-  dSlider = configureSlider(pSliderX, pSliderY + sliderDeltaY * 2);
-
+  pSlider = configureSlider(pSliderX, pSliderY, 0, 100, 0);
+  iSlider = configureSlider(pSliderX, pSliderY + sliderDeltaY, 0, 100, 0);
+  dSlider = configureSlider(pSliderX, pSliderY + sliderDeltaY * 2, 0, 100, 0);
+  setpointSlider = configureSlider(pSliderX, pSliderY + sliderDeltaY * 3, -720, 720, 0);
 }
